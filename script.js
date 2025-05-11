6f7df5ee2893d5d010a2b495ed5374b5efbd2ea4
Rakshita Goel <114772806+Rakshitag25@users.noreply.github.com>
2025-05-11 19:15:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -42,6 +42,13 @@ taskForm.addEventListener('submit', function (e) {
   localStorage.setItem("tasks", JSON.stringify(tasks));
   filterTasks(currentFilter);
   taskForm.reset();
+document.getElementById('task-deadline').value = new Date().toISOString().split('T')[0];
+
+const now = new Date();
+const hours = now.getHours().toString().padStart(2, '0');
+const minutes = now.getMinutes().toString().padStart(2, '0');
+document.getElementById('task-time').value = `${hours}:${minutes}`;
+
 });
 
 function addTaskToDOM(task) {
